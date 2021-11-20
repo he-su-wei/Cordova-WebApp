1f6066de13d45fed2d6371aa7d464a7b225b48cf
he-su-wei <johndoe@example.com>
2021-11-20 15:46:29 +0800

    new

---
+++ food_BlockChain_test/www/js/barcodescanner.js
@@ -0,0 +1,17 @@
+function scan(){
+    cordova.plugins.barcodeScanner.scan(
+        function(result){
+            if(!result.cancelled){
+                if(result.format == "QR_CODE") {
+                    var value = result.text;
+                    ws.send(value);  
+                    ws.send(localStorage.address);
+                }
+            }
+        },
+        function (error) {
+            alert('Scanning Failed '+error);
+        }
+    );
+}
+
